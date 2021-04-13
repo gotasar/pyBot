cd62fb796880ec855321c290643bc46200dd03e1
Orlanat <nikita_vasiliev@list.ru>
2021-04-13 22:34:07 +0300

    rework 6

--- bot_translator.py
+++ bot_translator.py
@@ -11,10 +11,42 @@ class bot_translator:
     @staticmethod
     def processing(message):
         user_id = message['message']['chat']['id']
+        user_name = message['message']['chat']['username']
+
+        if 'text' not in message['message']:
+            bot.send_message(user_id, f"Опять за старое взялся? {user_name}!", reply_markup=BotKeyboard.start_keyboard())
+            return
+
         text = message['message']['text']
+
+        if '/start' == text:
+            pass
+        elif 'Начать тест' in message:
+            pass
+        elif 'Статистика' in message:
+            pass
+        elif 'Настройка параметров' in message:
+            pass
+        elif 'Увеличить сложность' in message:
+            pass
+        elif 'Уменьшить сложность' in message:
+            pass
+        elif 'Увеличить повторения' in message:
+            pass
+        elif 'Уменьшить повторения' in message:
+            pass
+        elif 'Сменить тему' in message:
+            pass
+        elif 'Тема: ' in message:
+            pass
+        elif 'Ответ: ' in message:
+            bot.send_message(user_id, f"Я получил ответ", reply_markup=BotKeyboard.start_keyboard())
+            pass
+
         # Echo test without logic
         print(f"{bot} {text} {user_id}")
         bot.send_message(user_id, f"Эхо: {text}", reply_markup=BotKeyboard.start_keyboard())
         text = generate_test_question(conn, user_id)
         bot.send_message(user_id, f"Тестовый вопрос: {text}", reply_markup=BotKeyboard.start_keyboard())
+
 
