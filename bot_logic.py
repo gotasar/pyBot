d64c8be414e8e6051860f6a88b168fb45734eb26
Orlanat <nikita_vasiliev@list.ru>
2021-04-13 16:14:34 +0300

    rework 4

--- bot_logic.py
+++ bot_logic.py
@@ -1,4 +1,5 @@
 import random
+from datetime import datetime
 
 from tb_static import TB
 from db_static import DB
@@ -11,6 +12,13 @@ def generate_test_question(conn, user_id):
     cur = conn.cursor()
     cur.execute(f"SELECT * FROM users WHERE id = {user_id}")
     user_row = cur.fetchone()
+
+    # Pls delete this
+    if user_row is None:
+        add_user(user_id, "TestName")
+        cur.execute(f"SELECT * FROM users WHERE id = {user_id}")
+        user_row = cur.fetchone()
+
     # Найти слова по теме
     words = get_words(user_row)
     if words == -1:
@@ -47,4 +55,34 @@ def get_words(user_row):
     cur.execute(f"SELECT en, ru FROM words WHERE theme = {theme}")
     for row in cur:
         words.append({'EN': row[0], 'RU': row[1]})
+    print(f"Words: {words}")
     return words
+
+
+def add_user(id, first_name):
+    cur = conn.cursor()
+    cur.execute(f"SELECT * FROM users WHERE id = {id}")
+    row = cur.fetchone()
+    if row is not None:
+        return
+    cur.execute("INSERT INTO users (id, first_name, state, theme, complexity, grade, num_questions) " +
+                "VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                (id, first_name, 1, 1, 3, 3, 3))
+    conn.commit()
+    cur.execute("SELECT id FROM words")
+    for row in cur:
+        word_id = row[0]
+        print(word_id)
+        user_id = id
+        add_progress(user_id, word_id)
+    cur.execute("SELECT * FROM users")
+    for row in cur:
+        print(row)
+
+
+def add_progress(user, word):
+    cur = conn.cursor()
+    cur.execute("INSERT INTO progress (user_id, word, grade, last_date) " +
+                "VALUES (%s, %s, %s, %s)",
+                (user, word, 0, datetime.now()))
+    conn.commit()
