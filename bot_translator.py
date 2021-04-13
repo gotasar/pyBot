90adaf92fb389b9a73c3334985fa869be90ed7b5
Orlanat <nikita_vasiliev@list.ru>
2021-04-13 10:01:25 +0300

    rework 2

--- bot_translator.py
+++ bot_translator.py
@@ -1,4 +1,21 @@
+from tb_static import TB
+from db_static import DB
+
+bot = TB.bot()
+conn = DB.conn()
+
+
 class bot_translator:
     @staticmethod
-    def processing():
+    def processing(message):
+        user_id = message['message']['chat']['id']
+        bot.send_message(user_id, "Опять за старое взялся?")
         pass
+
+
+def send_message(bot_d, txt):
+    pass
+
+
+def test(bot_d, message):
+    pass
